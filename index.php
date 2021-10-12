8b826c5e985711d1e0cdb258e6b40a6d4c4680cc
Luciano <lucco6823@gmail.com>
2021-10-12 16:55:44 +0200

    Main e parte grafica terminata

--- index.php
+++ index.php
@@ -12,14 +12,34 @@
     <header>
         <img alt="Spotify logo" src="img/logo-spotify.png">
     </header>
-    <?php
-        foreach ($db as $details){
-            echo "<img src={$details['poster']} alt='poster {$details['title']}'>";
-            echo "<h2>" . $details["title"] . "<br>";
-            echo "<h3>" . $details["author"] . "<br>";
-            echo "<h3>" . $details["genre"] . "<br>";
-            echo "<h3>" . $details["year"] . "<br>";
-        }
-    ?>
+    <main>
+        <div class="main-dimension">
+            <div class="container-album">
+                <?php
+                    foreach ($db as $details){
+                        echo "
+                        <div class='album'>
+                            <div class='image'>
+                                <img src={$details['poster']} alt='poster {$details['title']}'>
+                                <h3 class='title'>
+                                    {$details['title']}
+                                </h3>
+                                <div>
+                                    <h5 class='author'>
+                                        {$details['author']}
+                                    </h5>
+                                    <h5 class='year'>
+                                        {$details['year']}
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                        ";
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
